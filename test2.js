6f9a8c1d25533f0b09783988434cec94dad7d87e
congcong564 <congcong0564@gmail.com>
2025-01-18 22:17:41 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -5,7 +5,7 @@ const body = $request.body;
 if (url.includes('/purchase/buynowui/buynow')) {
   // 处理表单 POST 请求
   if (body) {
-    const modifiedBody = body.replace(/products=[^&]*/, 'products=%5B%7B%22productId%22%3A%229NDWN4CBXPX7%22%2C%22skuId%22%3A%220010%22%2C%22availabilityId%22%3A%22B388XL464MZP%22%7D%5D');
+    const modifiedBody = body.replace(/products=[^&]*/, 'products=%5B%7B%22productId%22:%229NDWN4CBXPX7%22,%22skuId%22:%220010%22,%22availabilityId%22:%229TSS0LTH2Q26%22%7D%5D');
     // 将修改后的请求体重新赋值
     $done({ body: modifiedBody });
   } else {
@@ -15,7 +15,7 @@ if (url.includes('/purchase/buynowui/buynow')) {
   // 处理 JSON POST 请求
   if (body) {
     let jsonBody = JSON.parse(body);
-    jsonBody.items[0].availabilityId='B388XL464MZP'
+    jsonBody.items[0].availabilityId='9TSS0LTH2Q26'
     jsonBody.items[0].productId='9NDWN4CBXPX7'
     jsonBody.items[0].skuId='0010'
     // 将修改后的 JSON 字符串重新赋值给请求体
